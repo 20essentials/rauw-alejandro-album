cc17603a9081377afdba6dc14cbc4bd7961393db
DominicDev <s0l0estudi4r@gmail.com>
2026-03-20 17:31:47 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RAUW ALEJANDRO';
+export const nameSongs = [
+	"EL EFECTO ", /*1*/
+	"TODO DE TI", /*2*/
+	"FANTASÍAS", /*3*/
+	"TATTOO", /*4*/
+	"PUNTO 40", /*5*/
+	"ENCHULE", /*6*/
+	"CORAZÓN DESPEINADO", /*7*/
+	"DESENFOCADO", /*8*/
+	"CÚRAME", /*9*/
+	"LOKERA", /*10*/
+	"AL CALLAO", /*11*/
+	"RON COLA", /*12*/
+	"MUSEO", /*13*/
+	"DILE A ÉL", /*14*/
+	"NUBES", /*15*/
+	"HUNTER", /*16*/
+	"LA OLD SKUL", /*17*/
+	"AL MISMO TIEMPO", /*18*/
+	"QUIMICA", /*19*/
+	"NO ME SUELTES", /*20*/
+]
